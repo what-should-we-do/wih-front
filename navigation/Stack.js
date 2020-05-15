8bcf575da42cb384e7ebe9fbe7f6100ee87ca785
leeseonghyun <leeseonghyun21@gmail.com>
2020-05-16 05:08:51 +0900

    main screen with stack navigation

---
+++ navigation/Stack.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../screens/Home";
+import Category from "../screens/Category";
+
+const Stack = createStackNavigator();
+
+export default () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={Home} />
+  </Stack.Navigator>
+);
---
+++ screens/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+export default ({ navigation }) => (
+  <View>
+    <Text style={{ fontSize: 24 }}>메인 화면</Text>
+  </View>
+);
