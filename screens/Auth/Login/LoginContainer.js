26ad865edd0088319fdb71ce979adcac45a53610
leeseonghyun <leeseonghyun21@gmail.com>
2020-06-25 06:45:02 +0900

    [Login] added toast, alert

--- screens/Auth/Login/LoginContainer.js
+++ screens/Auth/Login/LoginContainer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Alert } from "react-native";
+import Toast from "react-native-root-toast";
 import LoginPresenter from "./LoginPresenter";
 import useInput from "../../../hooks/useInput";
 import { useLogIn } from "../../../AuthContext";
@@ -14,8 +16,17 @@ export default function LoginContainer({ navigation }) {
 
   async function onSubmit() {
     if (email.value === "test@gmail.com" && password.value === "1234") {
+      const toast = Toast.show("로그인 성공!", {
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.CENTER,
+        shadow: true,
+        animation: true,
+        hideOnPress: true,
+        delay: 0,
+      });
       login();
     } else {
+      Alert.alert("로그인", "아이디 또는 비밀번호가 틀렸습니다", [{ text: "OK" }]);
     }
   }
 
